5e7430d28a58f3813588d2527d3f69158518f55f
Vishnu Kumar Prajapati <75454756+vishnu95075@users.noreply.github.com>
2023-11-06 14:12:42 +0530

    Time: 3 ms (97.77%) | Memory: 10.7 MB (65.34%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.cpp
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -2,23 +2,28 @@ class Solution {
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n=rooms.size();
-        bool vis[n];
-        for(int i=0; i<n; i++)
-            vis[i] = false;
-        dfs(vis,rooms,0);
-
-        for(int i=0; i<rooms.size(); i++){
-            if(vis[i]==false) return false;
+        vector<bool> vis(n);
+        vis[0]=true;
+        queue<int> q;
+        for(auto &x: rooms[0]){
+            if(!vis[x]){
+                q.push(x);
+                vis[x]=true;
+            }
         }
-
+        while(!q.empty()){
+            int cur=q.front();
+            q.pop();
+            for(auto &x: rooms[cur]){
+                if(!vis[x]){
+                    q.push(x);
+                    vis[x]=true;
+                }
+            }
+        }
+        for(int i=0; i<n; i++){
+            if(!vis[i]) return false;
+        }
         return true;
-    }
-private:
-    void dfs(bool vis[],vector<vector<int>>& rooms,int i){
-        vis[i]=true;
-        for(auto x:rooms[i]){
-            if(!vis[x])
-                dfs(vis,rooms,x);
-        }
     }
 };
