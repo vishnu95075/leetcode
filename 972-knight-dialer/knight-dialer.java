a380b5ca2b76aed9cdebf8699c0e0bad56cf2131
Vishnu Kumar Prajapati <75454756+vishnu95075@users.noreply.github.com>
2023-11-27 22:26:06 +0530

    Time: 13 ms (94.84%) | Memory: 43 MB (80.71%) - LeetSync

--- 972-knight-dialer/knight-dialer.java
+++ 972-knight-dialer/knight-dialer.java
@@ -1,46 +1,42 @@
-class Solution {
-    int[][] memo;
-    int n;
-    int MOD = (int) 1e9 + 7;
-    int[][] jumps = {
-        {4, 6},
-        {6, 8},
-        {7, 9},
-        {4, 8},
-        {3, 9, 0},
-        {},
-        {1, 7, 0},
-        {2, 6},
-        {1, 3},
-        {2, 4}
-    };
-    
-    public int dp(int remain, int square) {
-        if (remain == 0) {
-            return 1;
-        }
-        
-        if (memo[remain][square] != 0) {
-            return memo[remain][square];
-        }
-        
-        int ans = 0;
-        for (int nextSquare : jumps[square]) {
-            ans = (ans + dp(remain - 1, nextSquare)) % MOD;
-        }
-        
-        memo[remain][square] = ans;
-        return ans;
-    }
-    
+class Solution {    
     public int knightDialer(int n) {
-        this.n = n;
-        memo = new int[n + 1][10];
+        int[][] jumps = {
+            {4, 6},
+            {6, 8},
+            {7, 9},
+            {4, 8},
+            {3, 9, 0},
+            {},
+            {1, 7, 0},
+            {2, 6},
+            {1, 3},
+            {2, 4}
+        };
+
+        int MOD = (int) 1e9 + 7;
+        int[] dp = new int[10];
+        int[] prevDp = new int[10];
+        Arrays.fill(prevDp, 1);
+
+        for (int remain = 1; remain < n; remain++) {
+            dp = new int[10];
+            for (int square = 0; square < 10; square++) {
+                int ans = 0;
+                for (int nextSquare : jumps[square]) {
+                    ans = (ans + prevDp[nextSquare]) % MOD;
+                }
+
+                dp[square] = ans;
+            }
+
+            prevDp = dp;
+        }
+
         int ans = 0;
         for (int square = 0; square < 10; square++) {
-            ans = (ans + dp(n - 1, square)) % MOD;
+            ans = (ans + prevDp[square]) % MOD;
         }
-        
+
         return ans;
     }
 }
