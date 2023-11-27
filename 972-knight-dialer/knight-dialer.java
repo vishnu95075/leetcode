f41a8a20f39aa18806fde5232f1e3986884ec217
Vishnu Kumar Prajapati <75454756+vishnu95075@users.noreply.github.com>
2023-11-27 22:26:44 +0530

    Time: 6 ms (98.37%) | Memory: 39.6 MB (94.29%) - LeetSync

--- 972-knight-dialer/knight-dialer.java
+++ 972-knight-dialer/knight-dialer.java
@@ -1,42 +1,29 @@
-class Solution {    
+class Solution {
     public int knightDialer(int n) {
-        int[][] jumps = {
-            {4, 6},
-            {6, 8},
-            {7, 9},
-            {4, 8},
-            {3, 9, 0},
-            {},
-            {1, 7, 0},
-            {2, 6},
-            {1, 3},
-            {2, 4}
-        };
-
+        if (n == 1) {
+            return 10;
+        }
+        
+        int A = 4;
+        int B = 2;
+        int C = 2;
+        int D = 1;
         int MOD = (int) 1e9 + 7;
-        int[] dp = new int[10];
-        int[] prevDp = new int[10];
-        Arrays.fill(prevDp, 1);
-
-        for (int remain = 1; remain < n; remain++) {
-            dp = new int[10];
-            for (int square = 0; square < 10; square++) {
-                int ans = 0;
-                for (int nextSquare : jumps[square]) {
-                    ans = (ans + prevDp[nextSquare]) % MOD;
-                }
-
-                dp[square] = ans;
-            }
-
-            prevDp = dp;
+        
+        for (int i = 0; i < n - 1; i++) {
+            int tempA = A;
+            int tempB = B;
+            int tempC = C;
+            int tempD = D;
+            
+            A = ((2 * tempB) % MOD + (2 * tempC) % MOD) % MOD;
+            B = tempA;
+            C = (tempA + (2 * tempD) % MOD) % MOD;
+            D = tempC;
         }
-
-        int ans = 0;
-        for (int square = 0; square < 10; square++) {
-            ans = (ans + prevDp[square]) % MOD;
-        }
-
-        return ans;
+        
+        int ans = (A + B) % MOD;
+        ans = (ans + C) % MOD;
+        return (ans + D) % MOD;
     }
 }
